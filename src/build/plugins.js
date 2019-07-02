1a60c40612db370a52aaf249bc19f366a29f20b2
Colton Colcleasure <colshacol@gmail.com>
2019-07-02 03:11:41 -0500

    Added image plugin

--- src/build/plugins.js
+++ src/build/plugins.js
@@ -2,10 +2,12 @@ import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import fileSize from 'rollup-plugin-filesize'
+import image from 'rollup-plugin-image'
 
 import { NAMED_EXPORTS } from '../consts'
 
 export const plugins = [
+  image(),
   fileSize(),
   babel({ runtimeHelpers: true }),
 
