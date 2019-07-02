656729e9e2d04de00903df948f99d8e3c3a0921c
Colton Colcleasure <colshacol@gmail.com>
2019-07-02 17:13:03 -0500

    Fixed src to source

--- src/consts.js
+++ src/consts.js
@@ -3,7 +3,7 @@ import path from 'path'
 export const CWD = process.cwd()
 export const PKG = require(`${CWD}/package.json`)
 export const EXTERNALS = ['react', 'react-dom']
-export const SOURCE_COMPONENTS_PATH = path.resolve(CWD, 'src/components')
+export const SOURCE_COMPONENTS_PATH = path.resolve(CWD, 'source/components')
 
 export const NAMED_EXPORTS = {
   react: [
